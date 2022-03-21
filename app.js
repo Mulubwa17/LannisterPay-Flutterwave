ecf0b0d4e88b1058f911e503150350d31aa8fded
Mulubwa Chungu <Mulubwac@gmail.com>
2022-03-21 15:27:22 +0200

    Completed assignment

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const compression = require('compression');
 const db = require('./config/db');
@@ -16,7 +15,6 @@ const options = {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cookieParser());
 app.use(cors());
 app.use(logger('combined'));
 app.use(helmet());
