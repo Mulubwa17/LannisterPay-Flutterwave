2813f34db36203f6691571ea5ad1e726c1ed60df
Mulubwa Chungu <Mulubwac@gmail.com>
2022-03-21 15:34:13 +0200

    Completed assignment

--- app.js
+++ app.js
@@ -23,8 +23,15 @@ app.use(cors(options));
 
 db.mainDb();
 
+app.get('/', function(req, res) {
+  res.send('LannisterPay assignment by Mulubwa Chungu!')
+});
+
 const Fees = require('./src/routes/transaction');
 
+
 app.use('/api', Fees);
+
+
 
 app.listen(3000, () => console.log('Connected on port 3000.'));
